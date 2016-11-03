e28f47913f8c94e8b66d2437277b0c252e09c733
Viet Le <lphuongviet@gmail.com>
2016-11-03 15:16:02 +0800

    Now can run React basic app

--- app/js/app.js
+++ app/js/app.js
@@ -1,1 +1,8 @@
-console.log('Fuck this bitch');
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Menu from './components/Global/Menu.jsx';
+
+ReactDOM.render(
+	<Menu />,
+	document.getElementById('app')
+);
